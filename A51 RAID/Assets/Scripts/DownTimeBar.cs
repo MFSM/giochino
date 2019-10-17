c8f96f684c6e5e9b62826425ff1e018854d20cc7
Cristian Tironi <tironi18@gmail.com>
2019-10-17 22:53:27 +0200

    DownTimeBar

---
+++ A51 RAID/Assets/Scripts/DownTimeBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DownTimeBar : MonoBehaviour
+{
+
+    private Transform bar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bar.localScale = new Vector3(3f, 0.3f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ A51 RAID/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour {
+
+    [SerializeField] private HealthBar healthBar;
+    float health = 1f;
+
+    private void Start () {
+
+        InvokeRepeating("DownTimeBar", 5f, 5f);
+	}
+
+    private void DownTimeBar()
+    {
+        
+        if (health > .01f)
+        {
+            health -= .01f;
+            healthBar.SetSize(health);
+
+            if (health < .3f)
+            {
+                // Under 30% health
+                if ((int)(health * 100f) % 3 == 0)
+                {
+                    healthBar.SetColor(Color.white);
+                }
+                else
+                {
+                    healthBar.SetColor(Color.red);
+                }
+            }
+        }
+        else
+        {
+            health = 1f;
+            healthBar.SetColor(Color.red);
+        }
+    }
+
+}
